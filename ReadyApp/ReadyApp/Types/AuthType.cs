04fc19c1785f47bba063f01bc6670a33340023a3
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-27 23:46:00 -0600

    Adding auth type enum resolve #16

---
+++ ReadyApp/ReadyApp/Types/AuthType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Types
+{
+    public enum AuthType
+    {
+        none,
+        ready,
+        google,
+        facebook
+    }
+}

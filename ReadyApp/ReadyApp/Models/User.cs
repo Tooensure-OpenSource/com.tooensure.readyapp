e31bdf7fc8655e3766cb289dfa09075fcc7909e3
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 17:14:08 -0600

    Creating user model object #10

---
+++ ReadyApp/ReadyApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using ReadyApp.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    public class User : ModelBase
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public User(string username, string email)
+        {
+            Username = username;
+            Email = email;
+        }
+    }
+}

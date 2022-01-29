047b935ce728f4de1d54c4116fb9fab6d516afb0
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 17:22:44 -0600

    Creating user model object business resolve #8

---
+++ ReadyApp/ReadyApp/Models/Business.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    public class Business : ModelBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public bool InProduction { get; set; } = false;
+
+    }
+}

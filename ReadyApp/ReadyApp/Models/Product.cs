dd6f51d71a808ff3df5213dae568abc32d0578cd
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 17:02:59 -0600

    Creating product model object resolve #12

---
+++ ReadyApp/ReadyApp/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    public class Product : ModelBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<ProductItem> ProductItems { get; set; } = new();
+        public decimal Cost { get; set; }
+    }
+}

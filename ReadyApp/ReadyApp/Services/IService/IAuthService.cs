9339f6cbc03095c7ca8219f1453b57e7b85695d4
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:30:37 -0600

    Pushing MvP interfaces

---
+++ ReadyApp/ReadyApp/Services/IService/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using ReadyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadyApp.Services.IService
+{
+    public interface IAuthService
+    {
+        Task<bool> SignInWithReady(string? email, string? password);
+        Task<bool> SignInWithGoogle();
+        Task<bool> SignInAnonymously();
+    }
+}
---
+++ ReadyApp/ReadyApp/Services/IService/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using ReadyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Services.IService
+{
+    public interface IUserService : IGenericService<User>
+    {
+    }
+}

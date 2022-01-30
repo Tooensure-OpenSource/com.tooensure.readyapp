ccad2aa625529d5c4d867314b12e9ba895627b38
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:02:40 -0600

    Adding base class resolve #22

---
+++ ReadyApp/ReadyApp/Models/ModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    /// <summary>
+    /// Hold the core base functionality which is to be shared
+    /// </summary>
+    public abstract class ModelBase
+    {
+        public string Id { get; set; } = string.Empty;
+    }
+}

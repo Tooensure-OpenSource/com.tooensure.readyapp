83c3ecb64e9f8d6af5e3779c8867179685804202
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:08:13 -0600

    Adding product model resolve #13

---
+++ ReadyApp/ReadyApp/Models/ProductItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    public class ProductItem : ModelBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; } = 1;
+    }
+}

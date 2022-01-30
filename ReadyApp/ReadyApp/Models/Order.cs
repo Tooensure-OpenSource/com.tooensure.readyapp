001dd54f31b14a99a0826c7a53cd5623eb14059c
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:47:30 -0600

    Adding order model resolve #34

---
+++ ReadyApp/ReadyApp/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using ReadyApp.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    public class Order : ModelBase
+    {
+        public string? businessId { get; set; }
+        public string? userId { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new();
+        public decimal Cost { get; set; }
+
+    }
+}

39014e8b4c052b2e0d27ad25ca68002bfcc952c0
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:37:44 -0600

    Adding auth type resolve #31

--- ReadyApp/ReadyApp/Types/AuthType.cs
+++ ReadyApp/ReadyApp/Types/AuthType.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace ReadyApp.Types
 {
+    /// <summary>
+    /// Creating auth type enum will specify what type if authentication the user wishes to authenticate themselves.
+    /// </summary>
     public enum AuthType
     {
         none,

eeb80c11dd73a35fc55e6237bb0732dbc7dbe468
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:19:41 -0600

    Adding Generic service interface and implementation resolve #25

---
+++ ReadyApp/ReadyApp/Services/GenericService.cs
@@ -0,0 +1,20 @@
+﻿using ReadyApp.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadyApp.Services
+{
+    public abstract class GenericService<T> : IGenericService<T> where T : class
+    {
+
+        public abstract Task<T?> Delete(string id);
+
+        public abstract Task<T?> Get(string id);
+
+        public abstract Task<IEnumerable<T>?> GetAll();
+
+        public abstract Task<T?> Update(T model);
+    }
+}
---
+++ ReadyApp/ReadyApp/Services/IService/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadyApp.Services.IService
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<T?> Get(string id);
+        Task<IEnumerable<T>?> GetAll();
+        Task<T?> Delete(string id);
+        Task<T?> Update(T model);
+    }
+}

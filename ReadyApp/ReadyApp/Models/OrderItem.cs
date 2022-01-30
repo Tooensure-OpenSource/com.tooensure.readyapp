2110acc024150d57fdeaff2f320cd339765f0787
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:50:54 -0600

    Adding order item resolve #35

---
+++ ReadyApp/ReadyApp/Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using ReadyApp.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    public class OrderItem : ModelBase
+    {
+        public Product Product { get; set; } = new();
+        public TransactionLevel GetTransaction { get; set; } = TransactionLevel.none;
+
+    }
+}

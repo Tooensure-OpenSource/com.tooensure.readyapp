cd87c8bf077fadf9a94f2aace58fb560a61af88f
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-29 18:44:16 -0600

    Adding UnitOfWork implementation and interface

---
+++ ReadyApp/ReadyApp/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using ReadyApp.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp
+{
+    public interface IUnitOfWork
+    {
+        IAuthService Auth { get; set; }
+    }
+}
---
+++ ReadyApp/ReadyApp/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using ReadyApp.Services;
+using ReadyApp.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        // Not to sure if I can cast here, but will see
+        public UnitOfWork()
+        {
+            Auth = (IAuthService)new AuthService();
+        }
+        public IAuthService Auth { get; set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

2952d19baa15caf4d1c8c1688c19562455acecdc
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-30 18:02:38 -0600

    Adding favorite model resolve #39

---
+++ ReadyApp/ReadyApp/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿using ReadyApp.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Models
+{
+    public class Notification : ModelBase
+    {
+        public string SourceId { get; set; } = String.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public NotificationType GetNotificationType { get; set; } = NotificationType.none;
+    }
+}

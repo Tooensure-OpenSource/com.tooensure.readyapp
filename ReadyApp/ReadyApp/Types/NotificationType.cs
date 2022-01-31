d8c51eb419d81f36c682c218dc7be46122dd751c
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-30 18:04:35 -0600

    Anding notification type resolve #38

---
+++ ReadyApp/ReadyApp/Types/NotificationType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadyApp.Types
+{
+    public enum NotificationType
+    {
+        none,
+        user,
+        business,
+        product,
+        productItem,
+        order,
+        orderItem
+    }
+}
